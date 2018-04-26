9f42a43d26bd2399f41fed33036370d17a279156
Sunny Ngan <sunny.ngan@buildium.com>
2018-04-26 16:41:31 -0400

    adjust a few more rules (#9)
    * adjust no mixed operators rule
    
    * 5.3.1-0
    
    * nopluplus change
    
    * 5.3.1-1

--- eslintrc.js
+++ eslintrc.js
@@ -71,6 +71,10 @@ module.exports = {
         }, {
           property: '__defineSetter__',
           message: 'Please use Object.defineProperty instead.',
-        }]
+        }],
+        // EX. allows var a = 1 + 2 - 3, but not var a = 1 && 2 || 3
+        "no-mixed-operators": ['error', {"allowSamePrecedence": true}],
+        // allow forloop to use ++ --
+        "no-plusplus": ['error', {"allowForLoopAfterthoughts": true}]
     }
 };
