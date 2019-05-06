eea79aac9342e1502e92e148e86d7cd49638a6a3
Calvin McEachron <digitil@users.noreply.github.com>
2019-05-06 14:17:13 -0400

    H2-1375 Update eslint config (#13)
    * update rules
    
    - adopt more es6 rules
    - use strings for severity levels

--- eslintrc.js
+++ eslintrc.js
@@ -1,82 +1,78 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "buildium"
+    extends: 'airbnb-base',
+    plugins: [
+        'buildium'
     ],
-    parserOptions: {
-        sourceType: 'script' //override airbnb-base
-    },
-    "rules": {
-        //our indentation
-        "indent": [2, 4, {"SwitchCase": 1}],
-        //don't require dangling comma
-        "comma-dangle": 0,
-        //use the global strict keyword
-        "strict": [2, "global"],
-        //allow return in else blocks
-        "no-else-return": 0,
-        //we can use a custom rule for func names
-        "func-names": 0,
-        //don't force a space after in a comment
-        "spaced-comment": 0,
-        "id-length": [2, {"min": 2, "properties": "never", "exceptions": ["$", "_", "i", "j", "k", "x", "y", "z"]}],
-        //we don't use one var keyword for all defs
-        "one-var": 0,
-        //we sometimes throw strings, which is ok
-        "no-throw-literal": 0,
-        //enforce radix
-        "radix": 2,
-        //warn when file doesn't end with newline
-        "eol-last": 1,
-        //we do .constant('blah',     require('blah')) a lot,
-        "no-multi-spaces": 0,
-        //VS doesn't clean up extra spaces on new lines, so this is annoying
-        "no-trailing-spaces": 0,
-        "no-param-reassign": [2, { "props": false }],
-        "space-before-function-paren": [2, "never"],
-        "import/no-unresolved": 0,
-        "no-underscore-dangle": 0,
-        "max-len": 0,
-        "global-require": 0,
-        "one-var-declaration-per-line": 0,
-        "object-curly-spacing": 0,
-        "quote-props": 0,
-        "newline-per-chained-call": 0,
-        "space-in-parens": 0,
-        "wrap-iife": [2, "any"],
-        "buildium/func-names": 2,
-        "buildium/template-location": 2,
-        "buildium/imports-lodash": 2,
-        "buildium/imports-jquery": 2,
-        "buildium/imports-angular-bsfy": 2,
-        "buildium/arrow-function-callback": 2,
-        "buildium/tagged-templates": 2,
-        //Overriding airbnb-base, we are not adapting these yet
-        "prefer-const": 0,
-        "prefer-arrow-callback": 0,
-        "linebreak-style": 0,
-        "arrow-parens": 0,
-        "import/newline-after-import": 0,
-        "import/no-extraneous-dependencies": 0,
-        //Our release build does not support object shorthand yet
-        "object-shorthand": ['error', 'never'],
-        "arrow-body-style": 0,
+    rules: {
+        // our indentation
+        indent: ['error', 4, { SwitchCase: 1 }],
+        // don't require dangling comma
+        'comma-dangle': 'off',
+        // use the global strict keyword
+        strict: ['warn', 'never'],
+        // allow return in else blocks
+        'no-else-return': 'off',
+        // don't force a space after in a comment
+        'spaced-comment': 'off',
+        'id-length': ['error', {
+            'min': 2,
+            'properties': 'never',
+            'exceptions': ['$', '_', 'i', 'j', 'k', 'x', 'y', 'z']
+        }],
+        // we don't use one var keyword for all defs
+        'one-var': 'off',
+        // we sometimes throw strings, which is ok
+        'no-throw-literal': 'off',
+        // enforce radix
+        'radix': 'error',
+        // warn when file doesn't end with newline
+        'eol-last': 'warn',
+        // we do .constant('blah',     require('blah')) a lot,
+        'no-multi-spaces': 'off',
+        // VS doesn't clean up extra spaces on new lines, so this is annoying
+        'no-trailing-spaces': 'off',
+        'no-param-reassign': ['error', { props: false }],
+        'space-before-function-paren': ['error', 'never'],
+        'no-underscore-dangle': 'off',
+        'max-len': 'off',
+        'global-require': 'off',
+        'one-var-declaration-per-line': 'off',
+        'object-curly-spacing': 'off',
+        'quote-props': 'off',
+        'newline-per-chained-call': 'off',
+        'space-in-parens': 'off',
+        'wrap-iife': ['error', 'any'],
+
+        // we use a custom rule for func names
+        'func-names': 'off',
+        'buildium/func-names': 'error',
+        'buildium/template-location': 'error',
+        'buildium/imports-lodash': 'error',
+        'buildium/imports-jquery': 'error',
+        'buildium/imports-angular-bsfy': 'error',
+        'buildium/arrow-function-callback': 'error',
+        'buildium/tagged-templates': 'error',
+        
+        // Overriding airbnb-base, we are not adopting these
+        'prefer-arrow-callback': 'off',
+        'linebreak-style': 'off',
+        'arrow-parens': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'arrow-body-style': 'off',
         'no-restricted-properties': ['error', {
-          object: 'arguments',
-          property: 'callee',
-          message: 'arguments.callee is deprecated',
+            object: 'arguments',
+            property: 'callee',
+            message: 'arguments.callee is deprecated',
         }, {
-          property: '__defineGetter__',
-          message: 'Please use Object.defineProperty instead.',
+            property: '__defineGetter__',
+            message: 'Please use Object.defineProperty instead.',
         }, {
-          property: '__defineSetter__',
-          message: 'Please use Object.defineProperty instead.',
+            property: '__defineSetter__',
+            message: 'Please use Object.defineProperty instead.',
         }],
         // EX. allows var a = 1 + 2 - 3, but not var a = 1 && 2 || 3
-        "no-mixed-operators": ['error', {"allowSamePrecedence": true}],
-        // allow forloop to use ++ --
-        "no-plusplus": ['error', {"allowForLoopAfterthoughts": true}],
-        "prefer-spread": 0,
-        "prefer-rest-params": 0
+        'no-mixed-operators': ['error', { allowSamePrecedence: true }],
+        // allow for-loop to use ++ --
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     }
 };
